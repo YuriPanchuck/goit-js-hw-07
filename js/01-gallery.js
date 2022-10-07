cfd8bc816b6249eb384333c7be5d881ecb45499e
YuriPanchuck <95381273+YuriPanchuck@users.noreply.github.com>
2022-10-07 23:45:08 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imgGalleryRef = document.querySelector(".gallery");
+const galleryMarkUp = createMakrUp(galleryItems);
+imgGalleryRef.innerHTML = galleryMarkUp;
+
+imgGalleryRef.addEventListener("click", onImageClick);
+
+function createMakrUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+let instance = "";
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instance = basicLightbox.create(
+    `
+    <img src=${event.target.dataset.source}>
+`
+  );
+  instance.show();
+
+  document.addEventListener("keydown", onEscCloseModal);
+}
+
+function onEscCloseModal(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    document.removeEventListener("keydown", onEscCloseModal);
+  }
+}

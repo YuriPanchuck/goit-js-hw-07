e74ff51aae7fe7294d5a7dd0d5cc1cdd6fbc9e5d
YuriPanchuck <95381273+YuriPanchuck@users.noreply.github.com>
2022-10-09 21:31:27 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkUp = createMarkUp(galleryItems);
+galleryRef.innerHTML = galleryMarkUp;
 
-console.log(galleryItems);
+galleryRef.addEventListener("click", onImageClick);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createMarkUp(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+function onImageClick(event) {
+  event.preventDefault();
+}
+
